190b23e94c2c647965a7fcb5c5128cc544ae8f44
j <to-json@proton.me>
2023-08-22 00:54:26 -0500

    more intermediate genericization

--- src/syscall_table.rs
+++ src/syscall_table.rs
@@ -1,6 +1,10 @@
 use std::{collections::HashMap, fs::File, ops::Index, path::PathBuf};
+use core::slice::Iter;
+
+use serde_json::Value;
 pub struct SyscallTable {
     call_map: HashMap<u64, String>,
+    _args: HashMap<String, Vec<String>>
 }
 impl Index<u64> for SyscallTable {
     type Output = String;
@@ -8,21 +12,31 @@ impl Index<u64> for SyscallTable {
         &&self.call_map[&idx]
     }
 }
+
+impl Index<String> for SyscallTable {
+    type Output = Vec<String>;
+    fn index(&self, idx: String) -> &Self::Output {
+        &&self._args[&idx]
+    }
+}
+
 impl SyscallTable {
     pub fn new(p: String) -> Result<SyscallTable, Box<dyn std::error::Error>> {
         let path = PathBuf::from(p);
         let json: serde_json::Value = serde_json::from_reader(File::open(path)?)?;
-        let call_map: HashMap<u64, String> = json["aaData"]
+        let json_iter: Iter<'_, Value> = json["aaData"]
             .as_array()
             .unwrap()
-            .iter()
-            .map(|item| {
-                (
-                    item[0].as_u64().unwrap(),
-                    item[1].as_str().unwrap().to_owned(),
-                )
+            .iter();
+        let _call_map: Option<HashMap<u64, String>> = json_iter.map(|item: &Value| -> Option<(u64, String)>{
+                Some((
+                    item.get(0)?.as_u64()?,
+                    item.get(1)?.as_str()?.to_owned(),
+                ))
             })
             .collect();
-        Ok(SyscallTable { call_map })
+        let call_map = _call_map.ok_or("Unable to parse call map")?;
+        let args: HashMap<String, Vec<String>> = Default::default();
+        Ok(SyscallTable { call_map, _args: args})
     }
 }
